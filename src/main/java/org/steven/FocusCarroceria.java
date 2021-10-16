ba566315b1f62421cb40647736951fc7f4cf3053
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-16 02:57:54 -0600

    string con datos del la carroceria

--- src/main/java/org/steven/FocusCarroceria.java
+++ src/main/java/org/steven/FocusCarroceria.java
@@ -1,4 +1,12 @@
 package org.steven;
 
-public class FocusCarroceria {
+public class FocusCarroceria extends Carroceria{
+
+    @Override
+    public String lcarroceria() {
+        System.out.println("características carroceria:\n");
+        String cceria="Color: Azul\nChasis: independiente\nCarroceria: Hard Top";
+
+        return cceria;
+    }
 }
--- src/main/java/org/steven/FocusMotor.java
+++ src/main/java/org/steven/FocusMotor.java
@@ -3,7 +3,7 @@ package org.steven;
 public class FocusMotor extends Motor{
     @Override
     public String pmotor() {
-        System.out.println("características:\n");
+        System.out.println("características motor:\n");
         String mtor="Código del motor: Ecoboost M1DA\nCapacidad: 998 (cc)\nTipo de distribución: Correa de distribución\nNúmero de cilindros: 3\nVálvulas por cilindro: 4\nDistancia entre bujías: 0,7 (mm)\nBoquilla: 0,24 – 0,30 (mm)";
         return mtor;
 
