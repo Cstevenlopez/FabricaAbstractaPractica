6eaa209c93d3c54907e7a84e756bafa0682df18e
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-16 02:34:31 -0600

    string con datos del motot

--- src/main/java/org/steven/FocusMotor.java
+++ src/main/java/org/steven/FocusMotor.java
@@ -1,4 +1,12 @@
 package org.steven;
 
-public class FocusMotor {
+public class FocusMotor extends Motor{
+    @Override
+    public String pmotor() {
+        System.out.println("características:\n");
+        String mtor="Código del motor: Ecoboost M1DA\nCapacidad: 998 (cc)\nTipo de distribución: Correa de distribución\nNúmero de cilindros: 3\nVálvulas por cilindro: 4\nDistancia entre bujías: 0,7 (mm)\nBoquilla: 0,24 – 0,30 (mm)";
+        return mtor;
+
+    }
+
 }
