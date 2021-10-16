dc8e70eb39965a9cf9502b95d758459dacbedf5d
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-16 03:17:21 -0600

    string con datos del motor astra

--- src/main/java/org/steven/AstraMotor.java
+++ src/main/java/org/steven/AstraMotor.java
@@ -1,2 +1,10 @@
-package org.steven;public class AstraMotor {
+package org.steven;
+
+public class AstraMotor extends Motor{
+    @Override
+    public String pmotor() {
+        System.out.println("características motor:\n");
+        String mtor="Posición del motor: Anterior, transversalmente\nCapacidad del motor: 1998 cm3\nPotencia máxima en: 5200 rpm\nTorsión: 170/2400 Nm\nSistema de combustible: Inyección multipunto\nNúmero de cilindros: 4";
+        return mtor;
+    }
 }
