9db78ea174f58ffaaf6ba93d7808368a53de6239
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-16 01:52:10 -0600

    Creacion de los metodos override

--- src/main/java/org/steven/FordFocusFactory.java
+++ src/main/java/org/steven/FordFocusFactory.java
@@ -1,4 +1,13 @@
 package org.steven;
 
-public class FordFocusFactory {
+public class FordFocusFactory implements CocheFactory{
+    @Override
+    public Motor crearMotor() {
+        return new FocusMotor();
+    }
+
+    @Override
+    public Carroceria crearCarroceria() {
+        return new FocusCarroceria();
+    }
 }
