518a52d37241dcfe646a6afe12eb2023f4317b01
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-16 00:30:45 -0600

    Creacion de el string de datos y los metodos get y set

--- src/main/java/org/steven/Carrocería.java
+++ src/main/java/org/steven/Carrocería.java
@@ -1,4 +1,15 @@
 package org.steven;
 
 public abstract class Carrocería {
+    private String Carroceria;
+
+    public abstract String lcarroceria();
+
+    public String getCarroceria(){
+        return Carroceria;
+    }
+
+    public void setCarroceria(String Carroceria){
+        this.Carroceria=Carroceria;
+    }
 }
