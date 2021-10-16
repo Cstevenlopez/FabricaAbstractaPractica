23a9f5aef8e85a4ae28f215f3a3bb2af756f3650
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-16 03:27:57 -0600

    string con datos de la carroceria de astra

--- src/main/java/org/steven/AstraCarroceria.java
+++ src/main/java/org/steven/AstraCarroceria.java
@@ -1,4 +1,10 @@
 package org.steven;
 
-public class AstraCarroceria {
+public class AstraCarroceria extends Carroceria{
+    @Override
+    public String lcarroceria() {
+        System.out.println("características carroceria:\n");
+        String cceria="Color: Verde\nChasis: independiente\nCarroceria: Hard Top";
+        return cceria;
+    }
 }
